2016b219feab31f1cb4f0bdb7c047e3091fff124
Akileri <eribabs1@gmail.com>
2022-07-04 14:28:51 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include<studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Writes to stdout with puts function.
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+   puts("Programming is like buildin a multilingual puzzle)
+ return (0);
+ }
