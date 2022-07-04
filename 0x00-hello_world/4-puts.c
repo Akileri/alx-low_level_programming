7e965cce02841c7e0a2e4c15f52be43455433e1b
Akileri <106988639+Akileri@users.noreply.github.com>
2022-07-04 22:57:47 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,10 @@
  * Description: Writes to stdout with puts function.
  *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
-   puts("Programming is like buildin a multilingual puzzle");
- return (0);
- }
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
