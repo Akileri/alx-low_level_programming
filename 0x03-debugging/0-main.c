987e95d6d833c42b98a2b13f54eeb550114cd467
Akileri <106988639+Akileri@users.noreply.github.com>
2022-07-09 05:29:43 +0100

    main c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,24 @@
+#include <stdio.h>
 #include "main.h"
 
+
+void positive_or_negative(int i){
+
+	/* my if statement */
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+
+}
 /**
 * main - tests function that prints if integer is positive or negative
 * return: 0
@@ -9,7 +28,7 @@ int main (void)
 {
   int i;
   
-  i = 98;
+  i = 0;
   positve_or_negative(i);
   
   return (0);
