b77e260f8cc5361e1395d8da4c90f7d2c8e49ea1
Akileri <106988639+Akileri@users.noreply.github.com>
2022-07-09 05:42:21 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-#include "main.h"
 
 
 void positive_or_negative(int i){
 	int n;
-	n = i:
 
-	/* my if statement */
-	if (n == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else if (n > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
-
+srand(time(0));
+n = i;
+if (n > 0)
+    printf("%d is positive\n", n);
+if (n == 0)
+    printf("%d is zero\n", n);
+if (n < 0)
+{
+   printf("%d is negative\n", n);
+}
+return (0);
 }
 /**
 * main - tests function that prints if integer is positive or negative
