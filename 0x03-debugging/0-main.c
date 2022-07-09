69ace57b81a45d15ba3185066f25d7d804ee0aea
Akileri <106988639+Akileri@users.noreply.github.com>
2022-07-09 05:31:25 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,13 +3,15 @@
 
 
 void positive_or_negative(int i){
+	int n;
+	n = i:
 
 	/* my if statement */
-	if (i == 0)
+	if (n == 0)
 	{
 		printf("%d is zero\n", i);
 	}
-	else if (i > 0)
+	else if (n > 0)
 	{
 		printf("%d is positive\n", i);
 	}
